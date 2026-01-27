8ea48b50fbe7d1e9b6286f5c3b231809b0b6054d
Luis Fernando <luisfer.andradesan@gmail.com>
2026-01-26 21:04:55 -0600

    const server

--- server_array.js
+++ server_array.js
@@ -1,6 +1,13 @@
 import http from "http";
 
-const server = http.createServer((req, res) => {});
+let numbers = [10, 20, 30];
+
+const server = http.createServer((req, res) => {
+    const {method, url} = req;
+    res.setHeader("Content-Type", "application/json");
+
+    
+});
 
 server.listen(3000, () => {
     console.log("Servidor escuchando...");
