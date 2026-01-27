1d3ee21b70ab9aa97549d8f40db9762dfcb3a45c
Luis Fernando <luisfer.andradesan@gmail.com>
2026-01-26 21:08:04 -0600

    initial if

--- server_array.js
+++ server_array.js
@@ -6,7 +6,9 @@ const server = http.createServer((req, res) => {
     const {method, url} = req;
     res.setHeader("Content-Type", "application/json");
 
-    
+    if (url.startsWith("/numbers")) { //    http://localhost:3000/numbers
+        const parts = url.split("/"); //    /numbers/2 = ["", "numbers", 2]
+    };
 });
 
 server.listen(3000, () => {
