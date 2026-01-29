931cd30900e9e2abf915717583b60cc369bdb0e7
Luis Fernando <luisfer.andradesan@gmail.com>
2026-01-28 20:53:09 -0600

    get

--- Clase-6/server.js
+++ Clase-6/server.js
@@ -10,6 +10,14 @@ app.get("/numbers", (req, res) => {
     res.status(200).json({numbers});
 });
 
+app.get("/numbers/:index", (req, res) => {
+    const index = parseInt(req.params.index);
+    if (isNaN(index) || index < 0 || index >= numbers.length) {
+        return res.status(404).json({error:"Índice no encontrado 404"});
+    }
+    res.json({ value: numbers[index] });
+});
+
 app.listen(3000, () => {
     console.log("Servidor ejecutándose en el puerto 3000");
 })
