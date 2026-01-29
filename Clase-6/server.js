a3b665d4518b09c532185f45b9be2bf16e103bd5
Luis Fernando <luisfer.andradesan@gmail.com>
2026-01-28 20:15:34 -0600

    Initial server.js

--- Clase-6/server.js
+++ Clase-6/server.js
@@ -1,3 +1,11 @@
 import express from "express";
 
 const app = express();
+
+app.use(express.json());
+
+let numbers = [10, 20, 30];
+
+app.get("/numbers", (req, res) => {
+    res.status(200).json({numbers});
+});
