ee5a18aa9fb3355ea22d0a60fc9dcfa3cd98d940
Luis Fernando <luisfer.andradesan@gmail.com>
2026-01-28 20:22:13 -0600

    firt server launch

--- Clase-6/server.js
+++ Clase-6/server.js
@@ -9,3 +9,7 @@ let numbers = [10, 20, 30];
 app.get("/numbers", (req, res) => {
     res.status(200).json({numbers});
 });
+
+app.listen(3000, () => {
+    console.log("Servidor ejecutándose en el puerto 3000");
+})
